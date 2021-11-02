0bf7ecf8b16c7f98898ea2a945f59a653882a5cb
miftahulazam <channelragil31@gmail.com>
2021-11-02 12:27:26 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h3>Materi Git Remote</h3>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, maiores odio? Laudantium nulla nihil recusandae consequatur dolore, officiis eos necessitatibus vero. Exercitationem laborum accusamus numquam omnis, quae aspernatur ratione eum!</p>
+    <ul>
+        <li>materi 1</li>
+        <li>materi 2</li>
+    </ul>
 </body>
 </html>
