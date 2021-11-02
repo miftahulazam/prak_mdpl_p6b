b2d22d7de18054086a569f14ade2d8e877d39a51
miftahulazam <channelragil31@gmail.com>
2021-11-02 12:19:10 +0700

    menambah paragraf

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MDPL_P6</title>
+</head>
+<body>
+    <h3>Materi Git Remote</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, maiores odio? Laudantium nulla nihil recusandae consequatur dolore, officiis eos necessitatibus vero. Exercitationem laborum accusamus numquam omnis, quae aspernatur ratione eum!</p>
+</body>
+</html>
